279d31d5b119a701f3bc3d121d15c9004a9b918c
Susa Milán <mailtosnek@protonmail.com>
2023-04-06 17:21:02 +0200

    Added poison + httpoison

--- mix.exs
+++ mix.exs
@@ -49,7 +49,9 @@ defmodule Gitty.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:poison, "~> 5.0"},
+      {:httpoison, "~> 2.1"},
     ]
   end
 
