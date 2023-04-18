794a93098ff9c59fe350522106d467ea002d3e21
Susa Milán <mailtosnek@protonmail.com>
2023-04-18 17:18:54 +0200

    get the profile associated with a username

--- lib/gitty_web/live/user/user_live.ex
+++ lib/gitty_web/live/user/user_live.ex
@@ -8,28 +8,29 @@ defmodule GittyWeb.User.UserLive do
 
   def render(assigns) do
     ~H"""
-    <h1>user: <%= @user %></h1>
-    <h4><%=  %></h4>
+    <h2><%= @user %></h2>
+    <h4><%= @profile.bio %></h4>
+    <%= if @profile.pfp do %>
+      <h5>M</h5>
+    <% end %>
     """
   end
 
   def mount(%{"user" => user}, _session, socket) do
     account = Repo.one!(from a in Accounts.User, where: a.username == ^user)
 
-    # profile = Repo.get(Profile, account)
+    profile =
+      Repo.one!(
+        from p in Profile,
+          join: a in Accounts.User,
+          where: p.user_id == a.id and a.id == ^account.id,
+          select: p
+      )
 
-    # profile =
-    #  Repo.one!(
-    #    from p in Profile,
-    #      join: a in Accounts.User,
-    #      where: p.user_id == a.id and a.id == ^account,
-    #      select: p
-    #  )
-
-    # IO.inspect(profile)
+    IO.inspect(profile)
 
     socket = assign(socket, user: user)
-    # socket = assign(socket, profile: profile)
+    socket = assign(socket, profile: profile)
 
     {:ok, socket}
   end
