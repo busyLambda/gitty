a1f2c751ee1bd496489ac46762c50c92adb1dc20
Susa Milán <mailtosnek@protonmail.com>
2023-04-20 11:01:40 +0200

    Added pfp to settings

--- lib/gitty_web/live/user/profile_settings_live.ex
+++ lib/gitty_web/live/user/profile_settings_live.ex
@@ -1,45 +1,70 @@
 defmodule GittyWeb.User.ProfileSettingsLive do
   use GittyWeb, :live_view
+  alias Gitty.Accounts
+  alias Gitty.Repo
+  alias Gitty.Profiles.Profile
+
+  alias Gitty.Profiles
+
+  import Ecto.Query
 
   def render(assigns) do
     ~H"""
-    <h4 class="mb-8"><%= @current_user.username %>'(s) settings</h4>
+    <div class="flex items-center">
+      <%= if @profile.pfp do %>
+        <h1>van</h1>
+      <% else %>
+        <%= raw(Profiles.gravatar(@email, 200, "w-48 rounded-lg mr-4 mb-2")) %>
+      <% end %>
+      <h4 class="mb-8 font-bold">
+        <%= @current_user.username %> - settings
+      </h4>
+    </div>
     <div class="flex w-full font-medium text-white">
       <div class="mr-3 w-auto">
         <ul class="font-bold">
-        <%= if @tab == "one" do %>
-          <li class="p-2 px-4 mb-2 rounded-lg bg-primary-500 text-surface-500">
-            show one
-          </li>
-        <% else %>
-          <li class="mb-2 rounded-lg bg-surface-600 hover:text-primary-500">
-            <button class="p-2 px-4" phx-click={JS.show(to: "#one") |> JS.push("switch_tab", value: %{tab: "one"})}>
+          <%= if @tab == "one" do %>
+            <li class="p-2 px-4 mb-2 rounded-lg bg-primary-500 text-surface-500">
               show one
-            </button>
-          </li>
-        <% end %>
-        <%= if @tab == "two" do %>
-          <li class="p-2 px-4 mb-2 rounded-lg bg-primary-500 text-surface-500">
-            show two
-          </li>
-        <% else %>
-          <li class="mb-2 rounded-lg bg-surface-600 hover:text-primary-500">
-            <button class="p-2 px-4" phx-click={JS.show(to: "#two") |> JS.push("switch_tab", value: %{tab: "two"})}>
+            </li>
+          <% else %>
+            <li class="mb-2 rounded-lg bg-surface-600 hover:text-primary-500">
+              <button
+                class="p-2 px-4"
+                phx-click={JS.show(to: "#one") |> JS.push("switch_tab", value: %{tab: "one"})}
+              >
+                show one
+              </button>
+            </li>
+          <% end %>
+          <%= if @tab == "two" do %>
+            <li class="p-2 px-4 mb-2 rounded-lg bg-primary-500 text-surface-500">
               show two
-            </button>
-          </li>
-        <% end %>
-        <%= if @tab == "three" do %>
-          <li class="p-2 px-4 rounded-lg bg-primary-500 text-surface-500">
-            show three
-          </li>
-        <% else %>
-          <li class="rounded-lg bg-surface-600 hover:text-primary-500">
-            <button class="p-2 px-4" phx-click={JS.show(to: "#three") |> JS.push("switch_tab", value: %{tab: "three"})}>
+            </li>
+          <% else %>
+            <li class="mb-2 rounded-lg bg-surface-600 hover:text-primary-500">
+              <button
+                class="p-2 px-4"
+                phx-click={JS.show(to: "#two") |> JS.push("switch_tab", value: %{tab: "two"})}
+              >
+                show two
+              </button>
+            </li>
+          <% end %>
+          <%= if @tab == "three" do %>
+            <li class="p-2 px-4 rounded-lg bg-primary-500 text-surface-500">
               show three
-            </button>
-          </li>
-        <% end %>
+            </li>
+          <% else %>
+            <li class="rounded-lg bg-surface-600 hover:text-primary-500">
+              <button
+                class="p-2 px-4"
+                phx-click={JS.show(to: "#three") |> JS.push("switch_tab", value: %{tab: "three"})}
+              >
+                show three
+              </button>
+            </li>
+          <% end %>
         </ul>
       </div>
       <div>
@@ -58,7 +83,22 @@ defmodule GittyWeb.User.ProfileSettingsLive do
   end
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, tab: "one")}
+    IO.inspect(socket)
+
+    account = socket.assigns.current_user
+
+    profile =
+      Repo.one!(
+        from p in Profile,
+          join: a in Accounts.User,
+          where: p.user_id == a.id and a.id == ^account.id,
+          select: p
+      )
+
+    socket = assign(socket, tab: "one")
+    socket = assign(socket, profile: profile)
+    socket = assign(socket, email: account.email)
+    {:ok, socket}
   end
 
   def handle_event("switch_tab", %{"tab" => tab}, socket) do
