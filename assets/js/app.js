d32ab27c1857bfb21ffc630361189924a306dc4a
Susa Milán <mailtosnek@protonmail.com>
2023-04-26 20:57:19 +0200

    Remove node_modules

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Prism from "prismjs"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
@@ -29,6 +30,9 @@ let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToke
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+
+Prism.highlightAll()
+console.log(Prism.plugins)
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
